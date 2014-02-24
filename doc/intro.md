50dc80548c579807885d5b5a7badbc4bb17d4c0d
Paul Williams <paul@paulswilliams.me.uk>
2014-02-24 07:50:05 +0000

    initial project creation with dependencies

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to octopress-scraper-to-ghost
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/octopress_scraper_to_ghost/core_test.clj
@@ -0,0 +1,7 @@
+(ns octopress-scraper-to-ghost.core-test
+  (:require [clojure.test :refer :all]
+            [octopress-scraper-to-ghost.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
