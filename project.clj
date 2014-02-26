8fff3f2be6eab336564ba9ec34386da3175f4cb9
Paul Williams <paul@paulswilliams.me.uk>
2014-02-26 12:58:04 +0000

    mid-merge

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [expectations "1.4.52"]
                  [enlive "1.1.5"]
-                 [clj-time "0.6.0"]])
+                 [clj-time "0.6.0"]
+                 [org.clojure/data.json "0.2.4"]])
--- src/octopress_scraper_to_ghost/core.clj
+++ src/octopress_scraper_to_ghost/core.clj
@@ -1,8 +1,10 @@
 (ns octopress-scraper-to-ghost.core
   (:require [net.cgrand.enlive-html :as html]
-            [clj-time.format :as date]))
+            [clj-time.format :as date]
+            [clojure.data.json :as json]))
 
 (def archive-relative "/blog/archives")
+(def template-path "ghost-import-template.json")
 
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
@@ -10,25 +12,37 @@
 (defn resolve-absolute [blog-url relative-a-tag]
   (str blog-url (:href (:attrs relative-a-tag))))
 
-(defn extract-blog-post [blog-post-url]
+(defn extract-blog-post [blog-post-url blog-post-template]
   (let [heading (first (html/select (fetch-url blog-post-url) [:article :h1]))
         time-published (first(html/select (fetch-url blog-post-url) [:time]))
-        body (first (html/select (fetch-url blog-post-url) [:div.entry-content]))]
-     { 
-       :title (html/text heading)
-       :slug (html/text heading)
-       :published_at (date/parse (date/formatters :date-time-no-ms) (:datetime (:attrs time-published)))
-       :html (apply str (html/emit* body))}))
+        body (first (html/select (fetch-url blog-post-url) [:div.entry-content]))]  blog-post-template 
+    { 
+     :title (html/text heading)
+     :slug (html/text heading)
+     :published_at (date/parse (date/formatters :date-time-no-ms) (:datetime (:attrs time-published)))
+     :html (apply str (html/emit* body))}))
 
 (defn jsonify-blog 
-  ([blog-url] (jsonify-blog blog-url (take 2 (html/select (fetch-url (str blog-url archive-relative)) [:div#blog-archives :a])) []))
-  ([blog-url blog-post-links blog-content]
+  ([blog-url] 
+    (let 
+      [json-template (json/read-json (slurp (clojure.java.io/resource template-path)))
+       blog-post-template (first (get-in json-template "data" "posts")) ]
+    (jsonify-blog 
+      blog-url 
+      (take 2 
+        (html/select 
+          (fetch-url (str blog-url archive-relative)) [:div#blog-archives :a]))
+      []
+      blog-post-template)))
+  ([blog-url blog-post-links blog-content blog-post-template]
    (if (seq blog-post-links)
      (recur blog-url 
-            (rest blog-post-links)
-            (conj  
-              blog-content 
-              (extract-blog-post (resolve-absolute blog-url (first blog-post-links)))))
+       (rest blog-post-links)
+       (conj  
+         blog-content 
+         (extract-blog-post 
+           (resolve-absolute blog-url (first blog-post-links)) blog-post-template))
+         blog-post-template)
      blog-content)))
 
 
